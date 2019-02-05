20152317cbe4cf599c55b2fe9c7f00d86dfdfe69
Goufix <alifer.17.11@gmail.com>
2019-02-05 20:43:00 -0200

    commit

--- src/screens/InputScreen.js
+++ src/screens/InputScreen.js
@@ -2,26 +2,60 @@ import React, { useState } from 'react';
 import { INPUT_SCREEN } from './SwitchScreen';
 import styles from '../styles/styles.css';
 
-export function write(response) {
-  const arrayResp = response.split('\n');
-  alert(arrayResp);
+export function Write(props) {
+  const arrayResp = props.data;
+  return <p>{arrayResp}</p>;
 }
 
 export default function InputScreen() {
   const [input, setInput] = useState('');
+  const [nick, setNick] = useState('');
+  const [position, setPosition] = useState('');
+  const [weekRef, setWeekRef] = useState('');
+  const [tag, setTag] = useState('');
   function count(e) {
-    let response = input;
+    return <Write data={input} />;
     e.preventDefault();
-    write(response);
   }
 
   function handleInput(e) {
     setInput(e.target.value);
   }
+
+  function handleNick(e) {
+    setNick(e.target.value);
+  }
+
+  function handleTag(e) {
+    setTag(e.target.value);
+  }
+
+  function handlePosition(e) {
+    setPosition(e.target.value);
+  }
+
+  function handleWeek(e) {
+    setWeekRef(e.target.value);
+  }
+
   return (
     <>
       <hr />
       <form onSubmit={count}>
+        <label>Nick:</label>
+        <input type="text" value={nick} onChange={handleNick} />
+        <br />
+        <label>Patente:</label>
+        <input type="text" value={position} onChange={handlePosition} />
+        <br />
+        <label>TAG:</label>
+        <input type="text" value={tag} onChange={handleTag} />
+        <br />
+        <label>Semana de Referência:</label>
+        <input type="text" value={weekRef} onChange={handleWeek} />
+        <br />
+        <label>Porcentagem Semanal:</label>
+        <br />
         <textarea value={input} onChange={handleInput} />
         <input type="submit" value="Gerar" />
       </form>
